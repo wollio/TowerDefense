82601f3d7ae555f085c04bf5637cc5b247d19cc6
David Wollschlegel <david@wollschlegel.com>
2015-02-23 22:46:39 +0100

    added some cutJs examples, which could be useful for the tower defense

---
+++ cutjsExamples/game-orbit/README.md
@@ -0,0 +1,78 @@
+# [Orbit](http://play.cutjs.org/examples/game-orbit/)
+
+Orbit is a cross-platform demo game for [CutJS](http://cutjs.org/)—the best HTML5 engine for cross-platform game development!  To package this game for mobile devices [Cordova 3](http://cordova.apache.org/) is used.
+
+[Play it here](http://play.cutjs.org/examples/game-orbit/)
+
+Basically this game consists of following files:
+- `www/index.html` the page viewing the game
+- `www/app.js` game code including logic and ui
+- `www/textures.js` definition of game graphic textures
+- `www/media/*` graphic image files
+
+In addition to the game files different CutJS builds are also needed for different platforms:
+- Copy `cut.web.js` to `www/js/cut.js`
+- Copy `cut.fc.js` to `merges/android/js/cut.js`
+- Copy `cut.cordova.js` to `merges/ios/js/cut.js`
+
+For other Cordova platforms do similar to iOS.
+
+Now all you need is to build your project.  But first make sure you have [installed NPM](https://www.google.com/search?q=install+node.js+npm) and then install Cordova 3 CLI:
+```
+npm install -g cordova
+```
+
+You don't need to create a new Cordova project, but if you like just copy above files after creating it.
+
+#### iOS
+
+First, add iOS platform to your Cordova project:
+```
+cordova platform add ios
+```
+
+Then, use Cordova `prepare` command to prepare your iOS project using `www` and `merges` folders and Cordova `config.xml` file:
+```
+cordova prepare ios
+```
+
+You iOS project is ready and available in `platforms/ios`, you can run or build it using Cordova or your own development kit.
+
+For other platforms you can follow similar steps.
+
+#### Android
+
+Android is also similar but requires an additional step. First, add Android platform to your Cordova project:
+```
+cordova platform add android
+```
+
+HTML5 Canvas usually doesn't perform well on Android, but there is a Corova plugin called [FastCanvas](https://github.com/phonegap/phonegap-plugin-fast-canvas) which we can use to shim it with a native GLSurface.
+Install [this edition](https://github.com/piqnt/phonegap-plugin-fast-canvas) of FastCanvas which is updated and optimized for CutJS:
+```
+cordova plugin add https://github.com/piqnt/phonegap-plugin-fast-canvas
+```
+Note that FastCanvas takes up entire screen and covers the HTML page.
+
+Then, prepare your Android project:
+```
+cordova prepare android
+```
+
+Now your Android project is ready, again, you can run or build it using Cordova or your own development kit.
+
+### Arts
+
+Graphic SVG files are included in `arts` folder.  You can export them manually or use [svgexport](https://github.com/shakiba/svgexport).
+
+First, install svgexport:
+```
+npm install svgexport -g
+```
+
+Once svgexport is installed, you can export arts:
+```
+svgexport art/game.json
+svgexport art/ios.json
+svgexport art/android.json
+```
--- js/app.js
+++ js/app.js
@@ -1,3 +1,120 @@
-/**
- * Created by David on 22.02.15.
- */
+function Game() {
+    var cells = [], turn = 0, ready = false;
+
+    this.ready = function() {
+        return ready;
+    };
+
+    this.init = function() {
+        console.log("Game.init");
+        cells = [];
+        for (var i = -1; i <= 1; i++) {
+            cells[i] = [];
+            for (var j = -1; j <= 1; j++) {
+                cells[i][j] = {
+                    i : i,
+                    j : j
+                };
+                this.uiNewCell(cells[i][j]);
+            }
+        }
+    };
+
+    this.start = function() {
+        console.log("Game.start");
+        if (cells.length == 0) {
+            this.init();
+        }
+        turn = 0, ready = true;
+        for (var i = -1; i <= 1; i++) {
+            for (var j = -1; j <= 1; j++) {
+                cells[i][j].sign = null;
+                this.uiUpdateCell(cells[i][j]);
+            }
+        }
+    };
+
+    this.click = function(cell) {
+        console.log("Game.click");
+        if (!ready || cell.sign) {
+            return;
+        }
+        cell.sign = (turn++ % 2 == 0) ? "o" : "x";
+        this.uiUpdateCell(cell);
+        var row = test(cell.i, cell.j, cell.sign);
+        if (row) {
+            ready = false;
+            this.uiWin(row, cell.sign);
+        } else if (turn >= 9) {
+            ready = false;
+            this.uiDraw(row, cell.sign);
+        }
+    };
+
+    function test(i, j, sign) {
+        console.log("Game.test");
+        if (cells[-1][j].sign == sign && cells[0][j].sign == sign
+            && cells[+1][j].sign == sign)
+            return [ cells[-1][j], cells[0][j], cells[+1][j] ];
+        if (cells[i][-1].sign == sign && cells[i][0].sign == sign
+            && cells[i][+1].sign == sign)
+            return [ cells[i][-1], cells[i][0], cells[i][+1] ];
+        if (i == j && cells[-1][-1].sign == sign && cells[0][0].sign == sign
+            && cells[+1][+1].sign == sign)
+            return [ cells[-1][-1], cells[0][0], cells[+1][+1] ];
+        if (i == -j && cells[-1][+1].sign == sign && cells[0][0].sign == sign
+            && cells[+1][-1].sign == sign)
+            return [ cells[-1][+1], cells[0][0], cells[+1][-1] ];
+    }
+
+}
+
+Cut(function(root, container) {
+    Cut.Mouse(root, container);
+    root.viewbox(50, 50).pin("handle", -0.5);
+
+    Cut.image("base:bg").pin("handle", 0.5).appendTo(root);
+
+    var game = new Game();
+
+    game.uiNewCell = function(cell) {
+        console.log("Game.uiNewCell");
+        cell.ui = Cut.image("base:x").appendTo(root).pin({
+            offsetX : cell.i * 10,
+            offsetY : cell.j * 10,
+            handle : 0.5
+        }).on("click", function() {
+            if (game.ready()) {
+                game.click(cell);
+            } else {
+                game.start();
+            }
+        });
+    };
+
+    game.uiUpdateCell = function(cell) {
+        console.log("Box.uiUpdateCell");
+        cell.ui.image("base:" + (cell.sign || "-"));
+        cell.ui.pin({
+            alpha : 0.8,
+            scale : 1
+        });
+    };
+
+    game.uiWin = function name(row, sign) {
+        console.log("Game.uiWin");
+        for (var i = 0; i < row.length; i++) {
+            var tween = row[i].ui.tween(200).pin({
+                alpha : 1,
+                scaleX : 1.2,
+                scaleY : 1.2
+            });
+        }
+    };
+
+    game.uiDraw = function() {
+        console.log("Game.uiDraw");
+    };
+
+    game.start();
+});
