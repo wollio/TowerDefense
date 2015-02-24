62883c2e461ac12b07c5b133d95062bcf4a2b62b
SEVCO <severin.putzi@gmail.com>
2015-02-24 15:42:28 +0100

    edited Readme

--- js/app.js
+++ js/app.js
@@ -3,20 +3,14 @@ Cut(function(root, container) {
     Cut.Mouse(root, container);
 
     root.viewbox(200, 200);
-
-    var width = 50, height = 50, r1 = 10, r2 = 20;
-
-    var image = Cut.image().appendTo(root).pin("align", 0.5);
-
     root.on(Cut.Mouse.CLICK, function() {
         draw();
     });
 
-    var p = 4;
     draw();
 
     function draw() {
-        var tower = new MagicTower(5,5,10,10,root);
-        tower.draw();
+        var magicTower = new MagicTower(5,5,root);
+        magicTower.draw();
     }
 });
--- js/model/tower.js
+++ js/model/tower.js
@@ -1,4 +1,4 @@
-function Tower(xPos, yPos,image, attackspeed, range, damage) {
+function Tower(xPos, yPos, image, attackspeed, range, damage) {
     this.attackspeed = attackspeed;
     this.damage = damage;
     this.range = range;
@@ -6,11 +6,9 @@ function Tower(xPos, yPos,image, attackspeed, range, damage) {
     this.y = yPos;
     this.image = image;
     this.draw = function() {
-        var x = this.x;
-        var y = this.y;
-        this.image.image(Cut.Out.drawing(this.width, this.height, 4, function (ctx, ratio) {
+        this.image.image(Cut.Out.drawing(20, 20, 4, function (ctx, ratio) {
             ctx.scale(ratio, ratio);
-            ctx.arc(x, y, 2.4, 0, 2 * Math.PI);
+            ctx.arc(5, 5, 2.4, 0, 2 * Math.PI);
             ctx.lineWidth = 0.5;
             ctx.strokeStyle = "#000";
             ctx.stroke();
@@ -20,13 +18,13 @@ function Tower(xPos, yPos,image, attackspeed, range, damage) {
 
 
 
-function MagicTower(xPos, yPos, root) {
+function MagicTower(x, y, root) {
     this.attackspeed = 0.5;
     this.damage = 10;
     this.range = 5;
     this.root = root;
-    this.image = Cut.image().appendTo(root).pin("align", 0.5);
-Tower.call(this,xPos,yPos,this.image,this.attackspeed, this.range,this.damage);
+    this.image = Cut.image().appendTo(this.root).pin("align", 0.5);
+    Tower.call(this,x,y,this.image,this.attackspeed, this.range,this.damage);
 }
 
 function FireTower(xPos, yPos, root) {
