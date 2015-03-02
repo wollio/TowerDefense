656a6c1a3cacca5ff613f13873ab295385b5d3d7
David Wollschlegel <david@wollschlegel.com>
2015-03-02 19:58:02 +0100

    added classexample

--- js/model/monster.js
+++ js/model/monster.js
@@ -1,11 +1,22 @@
-function Monster(first, last) {
-    this.first = first;
-    this.last = last;
+function Monster(x, y, health, speed) {
+    this.x = x;
+    this.y = y;
+    this.health = health;
+    this.speed = speed;
+    this.move = function() {
+        this.x = this.x + this.speed;
+    };
+    this.draw = function() {
+        var x = this.x;
+        var y = this.y;
+        this.image.image(Cut.Out.drawing(this.width, this.height, 4, function (ctx, ratio) {
+            ctx.scale(ratio, ratio);
+            ctx.arc(x, y, 2.4, 0, 2 * Math.PI);
+            ctx.lineWidth = 0.5;
+            ctx.strokeStyle = "#FF3870";
+            ctx.stroke();
+        }));
+    };
 }
 
-Monster.prototype.fullName = function() {
-    return this.first + ' ' + this.last;
-};
-Monster.prototype.fullNameReversed = function() {
-    return this.last + ', ' + this.first;
-};
+
